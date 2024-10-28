776baed325d25690ab8b4182bb5ffd73b4f9dae4
Kris Hofman <khofman250377@gmail.com>
2024-10-28 18:37:15 +0100

    chore  fibonacci with custom coroutine generator

--- Coroutines/src/co_generator.hpp
+++ Coroutines/src/co_generator.hpp
@@ -1,3 +1,7 @@
+#ifndef CO_GENERATOR_HPP_
+#define CO_GENERATOR_HPP_
+
+#include "custom_coroutine_handle.hpp"
 #include <coroutine>
 // std23
 #include <generator>
@@ -28,4 +32,69 @@ std::generator<int> Fibonacci(int n) {
   }
 }
 
+template<typename T>
+struct CustomGeneratorPromise;
+
+template<typename T>
+struct CustomGenerator {
+  using promise_type = CustomGeneratorPromise<T>;
+
+  explicit CustomGenerator(promise_type::handle_ handle) : handle_(handle) {}
+
+  // Let coroutine generator yield next result, if the coroutine completetion. 
+  bool exhausted() {
+    if (not handle_.done()) {
+      handle_.resume();
+    }
+    return handle_.done();
+  }
+
+  T get() {
+    return handle_.promise().get();
+  }
+private:
+  CustomCoroutineHandle::CoroutineHandle<promise_type> handle_;
+};
+
+template<typename T>
+struct CustomGeneratorPromise {
+  using handle_ = std::coroutine_handle<CustomGeneratorPromise<T>>;
+
+  // Customization coroutine point: happend before the coroutine body starts.
+  // Immediate return result to the caller.
+  std::suspend_always initial_suspend() noexcept {
+    return {};
+  }
+
+  // Customization coroutine point: happend after the coroutine body finishs.
+  // std::suspend_always here is a caller to read the result,
+  // the coroutine now cannot finish after and coroutine_handle.destroy leads
+  // into SIGSEGV after completed executing block.
+  std::suspend_always final_suspend() noexcept {
+    return {};
+  }
+
+  CustomGenerator<T> get_return_object() {
+    return CustomGenerator<T>(handle_::from_promise(*this));
+  }
+
+  template<std::convertible_to<T> Arg>
+  std::suspend_always yield_value(Arg&& value) {
+    result_ = std::forward<Arg>(value);
+    return {};
+  }
+
+  // TODO: void return value here?
+  void return_void() {}
+  void unhandled_exception() {}
+  
+  T get() const {
+    return result_;
+  }
+
+private: 
+  T result_;
+};
+  
 } // namespace
+#endif // CO_GENERATOR_HPP_
--- Coroutines/src/main.cpp
+++ Coroutines/src/main.cpp
@@ -1,54 +1,97 @@
 
 #include "co_generator.hpp"
+#include "co_lazy_function.hpp"
 #include "co_tree_traversal.hpp"
 #include "custom_coroutine.hpp"
 #include <print>
 
+void fibonaccii_std_generator(int n) {
+std::println("Fibonacci generator: ");
+  for (int fib : CoGenerator::Fibonacci(n)) {
+    std::print("{} ", fib);
+  }
+  std::println();
+}
+
+
+void fibonacci_custom_generatpr(int n) {
+  auto fibonacci = [](int limit) -> CoGenerator::CustomGenerator<int> {
+    int prev = 0;
+    int next = 1;
+    for (;limit > 0; --limit) {      
+        co_yield std::exchange(prev, std::exchange(next, prev + next));
+    }
+    co_return;
+  };
+  
+  std::println("Fibonacci algorithm with custom coroutine generator: ");
+  auto res = fibonacci(n);
+  while (not res.exhausted()){
+    std::print("{} ", res.get());
+  }
+  std::println();
+}
+
+void co_tree_tranversal() {
+auto tree = CoTreeTranversal::BuildTree();
+  auto root = tree.front().get();
+
+  std::println("Tree traversal with coroutines");
+  std::println("---CoPreOrder: ");
+  for (auto node : CoTreeTranversal::CoPreOrder(root)) {
+    std::print("{} ", node->value);
+  }
+  std::println("");
+
+  std::println("---CoInOrder: ");
+  for (auto node : CoTreeTranversal::CoInOrder(root)) {
+    std::print("{} ", node->value);
+  }
+  std::println("");
+
+  std::println("---CoPostOrder: ");
+  for (auto node : CoTreeTranversal::CoPostOrder(root)) {
+    std::print("{} ", node->value);
+  }
+  std::println();
+}
+
+void custom_coroutine() {
+// Custtom coroutine
+  std::println("Custom coroutine");    
+  auto coro = [] -> CustomCoroutine::Coroutine<int> {
+    throw std::runtime_error("Runtime exception...");
+    co_return 123;
+  };    
+
+  try {
+    auto res = coro();
+    std::println("Custom coroutine result: {}", res.get());
+    // The same above
+    std::println("Custom coroutine result: {}\n", coro().get());
+  }
+  catch (const std::exception& e) {
+  }
+}
+
+void lazy_coroutine(){
+  // Lazy coroutine
+  auto lazyCoro = [] -> LazyCoro::LazyCoroutine<int> {
+    std::println("Running lazy coroutine...");
+    co_return 345;
+  };
+
+  // Coroutine instantation and the result is pass to the caller 
+  auto x = lazyCoro();
+  auto res = x.get();
+  auto res2 = x.get();
+  std::println("res = {}, res2 = {}\n", res, res2);        
+}
+
 int main() {
-  {
-    std::println("Fibonacci generator: ");
-    for (int fib : CoGenerator::Fibonacci(10)) {
-      std::println("{}", fib);
-    }
-  }
-  {
-    auto tree = CoTreeTranversal::BuildTree();
-    auto root = tree.front().get();
-
-    std::println("CoPreOrder: ");
-    for (auto node : CoTreeTranversal::CoPreOrder(root)) {
-      std::print("{} ", node->value);
-    }
-    std::println("");
-
-    std::println("CoInOrder: ");
-    for (auto node : CoTreeTranversal::CoInOrder(root)) {
-      std::print("{} ", node->value);
-    }
-    std::println("");
-
-    std::println("CoPostOrder: ");
-    for (auto node : CoTreeTranversal::CoPostOrder(root)) {
-      std::print("{} ", node->value);
-    }
-    std::println("");
-  }  
-
-  {
-    // Custtom coroutine
-    std::println("Custom coroutine");    
-    auto coro = [] -> CustomCoroutine::Coroutine<int> {
-      throw std::runtime_error("Runtime exception...");
-      co_return 123;
-    };    
-
-    try {
-      auto res = coro();
-      std::println("Custom coroutine result: {}", res.get());
-      // The same above
-      std::println("Custom coroutine result: {}", coro().get());
-    }
-    catch (const std::exception& e) {
-    }
-  }
+  fibonaccii_std_generator(10);
+  fibonacci_custom_generatpr(10);
+  co_tree_tranversal();
+  custom_coroutine();
+  lazy_coroutine();
 }
